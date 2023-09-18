5477f54e0c04b16627d1971ae196f69c94059d62
Jem <0x0xjem@gmail.com>
2023-09-18 16:58:59 +0400

    Add scheduled generation, Firebase URL

--- pulumi.ts
+++ pulumi.ts
@@ -6,6 +6,8 @@ import { handleGet } from "./src/functions/get";
 
 const gcpConfig = new pulumi.Config("gcp");
 // const pulumiConfig = new pulumi.Config("pulumi");
+const projectName = "cooler-loans-api";
+const projectStackName = `${projectName}-${pulumi.getStack()}`;
 
 /**
  * Enable services
@@ -19,9 +21,12 @@ const serviceCloudBuild = new gcp.projects.Service("cloudbuild", {
 const serviceCloudFunctions = new gcp.projects.Service("cloudfunctions", {
   service: "cloudfunctions.googleapis.com",
 });
-// const serviceCloudScheduler = new gcp.projects.Service("cloudscheduler", {
-//   service: "cloudscheduler.googleapis.com",
-// });
+const serviceCloudScheduler = new gcp.projects.Service("cloudscheduler", {
+  service: "cloudscheduler.googleapis.com",
+});
+const serviceFirebase = new gcp.projects.Service("firebase", {
+  service: "firebase.googleapis.com",
+});
 
 /**
  * Create Firestore database
@@ -39,7 +44,7 @@ new gcp.firestore.Database(
 );
 
 /**
- * Deploy Cloud Function - Generate
+ * Deploy Cloud Function - Generate daily snapshots
  */
 const functionGenerate = new gcp.cloudfunctions.HttpCallbackFunction(
   "generate",
@@ -57,7 +62,21 @@ const functionGenerate = new gcp.cloudfunctions.HttpCallbackFunction(
   },
 );
 
-// TODO Schedule it
+// Create a Cloud Scheduler job to run every hour
+new gcp.cloudscheduler.Job(
+  "generate",
+  {
+    schedule: "0 * * * *",
+    timeZone: "UTC",
+    httpTarget: {
+      uri: functionGenerate.httpsTriggerUrl,
+      httpMethod: "GET",
+    },
+  },
+  {
+    dependsOn: [serviceCloudScheduler, functionGenerate],
+  },
+);
 
 /**
  * Deploy Cloud Function - Fetch
@@ -73,5 +92,76 @@ const functionGet = new gcp.cloudfunctions.HttpCallbackFunction(
   },
 );
 
+/**
+ * Firebase
+ *
+ * We utilise Firebase hosting to provide a static URL to the Cloud Function.
+ */
+
+// Deploy a Firebase Hosting site, so that we can obtain a static URL
+const firebaseProject = new gcp.firebase.Project(
+  projectName,
+  {
+    project: gcpConfig.require("project"),
+  },
+  {
+    dependsOn: [serviceFirebase],
+  },
+);
+
+const firebaseHostingSite = new gcp.firebase.HostingSite(
+  projectName,
+  {
+    project: firebaseProject.project,
+    siteId: `olympusdao-${projectStackName}`, // Will end up as olympusdao-cooler-loans-api-<stack>.web.app
+  },
+  {
+    dependsOn: [firebaseProject],
+  },
+);
+
+const firebaseSiteId = firebaseHostingSite.siteId;
+if (!firebaseSiteId) {
+  throw new Error("Firebase Hosting site ID is undefined");
+}
+
+const firebaseSiteIdInput: pulumi.Input<string> = firebaseSiteId.apply(str => `${str}`);
+
+// Create a rewrite rule to redirect all requests to the Cloud Function
+const firebaseHostingVersion = new gcp.firebase.HostingVersion(
+  projectName,
+  {
+    siteId: firebaseSiteIdInput,
+    config: {
+      redirects: [
+        {
+          glob: "/",
+          location: functionGet.httpsTriggerUrl,
+          statusCode: 302,
+        },
+      ],
+    },
+  },
+  {
+    dependsOn: [firebaseHostingSite, functionGet],
+  },
+);
+
+new gcp.firebase.HostingRelease(
+  projectName,
+  {
+    siteId: firebaseSiteIdInput,
+    versionName: firebaseHostingVersion.name,
+    message: "Cloud Functions integration",
+  },
+  {
+    dependsOn: [firebaseHostingVersion],
+  },
+);
+
+/**
+ * Exports
+ */
 export const generateHttpsUrl = functionGenerate.httpsTriggerUrl;
 export const getHttpsUrl = functionGet.httpsTriggerUrl;
+export const firebaseHostingUrl = firebaseHostingSite.defaultUrl;
