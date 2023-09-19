d8e84c166094568d448276b5acf0de264b0fe3f8
Jem <0x0xjem@gmail.com>
2023-09-19 14:14:14 +0400

    Fix for CORS errors

--- pulumi.ts
+++ pulumi.ts
@@ -1,5 +1,7 @@
 import * as gcp from "@pulumi/gcp";
 import * as pulumi from "@pulumi/pulumi";
+import cors from "cors";
+import express from "express";
 
 import { handleGenerate } from "./src/functions/generate";
 import { handleGet } from "./src/functions/get";
@@ -35,7 +37,8 @@ new gcp.firestore.Database(
   "default",
   {
     name: "(default)",
-    locationId: gcpConfig.require("region"),
+    // us-central1 is not supported, so this is used instead
+    locationId: "us-east1",
     type: "FIRESTORE_NATIVE",
   },
   {
@@ -81,11 +84,32 @@ new gcp.cloudscheduler.Job(
 /**
  * Deploy Cloud Function - Fetch
  */
+const corsOrigins = ["https://app.olympusdao.finance/", "https://staging.olympusdao.finance", "https://*.fleek.co"];
+if (pulumi.getStack() === "dev") {
+  corsOrigins.push("http://localhost:5173");
+}
+
 const functionGet = new gcp.cloudfunctions.HttpCallbackFunction(
   "get",
   {
     runtime: "nodejs18",
-    callback: handleGet,
+    callbackFactory: () => {
+      /**
+       * Google Cloud Functions mangles the handling of CORS, so we
+       * wrap it in an Express app that is configured to handle CORS
+       * and applies restrictions to the origin.
+       */
+      const app = express();
+      app.use(
+        cors({
+          origin: corsOrigins,
+        }),
+      );
+
+      app.get("/", handleGet);
+
+      return app;
+    },
   },
   {
     dependsOn: [serviceFirestore, serviceCloudBuild, serviceCloudFunctions],
@@ -113,7 +137,7 @@ const firebaseHostingSite = new gcp.firebase.HostingSite(
   projectName,
   {
     project: firebaseProject.project,
-    siteId: `olympusdao-${projectStackName}`, // Will end up as olympusdao-cooler-loans-api-<stack>.web.app
+    siteId: `olympus-${projectStackName}`, // Will end up as olympus-cooler-loans-api-<stack>.web.app
   },
   {
     dependsOn: [firebaseProject],
@@ -127,17 +151,25 @@ if (!firebaseSiteId) {
 
 const firebaseSiteIdInput: pulumi.Input<string> = firebaseSiteId.apply(str => `${str}`);
 
-// Create a rewrite rule to redirect all requests to the Cloud Function
+// Rewrite all requests to the Cloud Function
 const firebaseHostingVersion = new gcp.firebase.HostingVersion(
   projectName,
   {
     siteId: firebaseSiteIdInput,
     config: {
-      redirects: [
+      /**
+       * Firebase hosting does not forward CORS headers to the Cloud Function
+       * when using redirects, so we need to do a rewrite.
+       *
+       * Pulumi's implementation does not support specifying the region of the
+       * function (or does not discover the region accurately), so we need to
+       * ensure that both the Cloud Function and Firebase Hosting are in the
+       * default region, which is us-central1.
+       */
+      rewrites: [
         {
-          glob: "/",
-          location: functionGet.httpsTriggerUrl,
-          statusCode: 302,
+          glob: "**",
+          function: functionGet.function.name,
         },
       ],
     },
