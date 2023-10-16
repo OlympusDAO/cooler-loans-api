9a3aadf9310f4436642632c26bc13b720bc8d058
Jem <0x0xjem@gmail.com>
2023-10-16 12:01:30 +0400

    Have codegen use the Pulumi-defined subgraph endpoint

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,28 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import * as dotenv from "dotenv";
+import fs from "fs";
+import { parse } from "yaml";
+
+// Read .env file
+dotenv.config();
+
+const resolveSubgraphUrl = (): string => {
+  if (!process.env.SUBGRAPH_API_KEY) {
+    throw new Error(
+      `"SUBGRAPH_API_KEY environment variable is not set. Please set it to your Subgraph API key to query subgraphs"`,
+    );
+  }
+
+  // Parse the Pulumi config file
+  const pulumiConfig = parse(fs.readFileSync("./Pulumi.dev.yaml", "utf-8"));
+  const url = pulumiConfig.config["cooler-loans-api:GRAPHQL_ENDPOINT"] as string;
+
+  return url.replace("[api-key]", process.env.SUBGRAPH_API_KEY);
+};
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://api.studio.thegraph.com/query/46563/cooler-loans/1.4.3",
+  schema: resolveSubgraphUrl(),
   documents: "./src/**/*.graphql",
   generates: {
     "src/generated/graphql.ts": {
