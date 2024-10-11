16a44c2391a9aef3e914fa8a1b429f7a0126e048
Jem <0x0xjem@gmail.com>
2024-10-11 11:10:55 +0400

    Shift to bundling of app code before upload to Google Cloud Functions

--- apps/generate/tsup.config.ts
+++ apps/generate/tsup.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src", "!src/**/__tests__/**"],
-  noExternal: ["@repo/shared", "@repo/types"],
-  // format: ["cjs"],
+  // https://github.com/egoist/tsup/issues/619
+  noExternal: [ /(.*)/ ],
   clean: true,
   dts: true,
+  minify: true,
   target: "node18",
 });
--- packages/infrastructure/src/cloudFunction.ts
+++ packages/infrastructure/src/cloudFunction.ts
@@ -1,5 +1,29 @@
 import * as gcp from "@pulumi/gcp";
 import * as pulumi from "@pulumi/pulumi";
+import { mkdirSync, readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+import * as fsExtra from "fs-extra";
+
+const prepareTemporaryDirectory = (name: string, distDirectory: string) => {
+  // Create a temporary directory
+  const tempDirectory = join("../../tmp", name);
+  mkdirSync(tempDirectory, { recursive: true });
+  console.log(`Created temporary directory ${tempDirectory}`);
+
+  // Construct a dummy package.json
+  const packageJson = {
+    name,
+    main: "dist/index.js",
+  };
+
+  // Write a package.json to the temporary directory
+  writeFileSync(join(tempDirectory, "package.json"), JSON.stringify(packageJson, null, 2));
+
+  // Copy the dist directory to the temporary directory
+  fsExtra.copySync(distDirectory, join(tempDirectory, "dist"));
+
+  return tempDirectory;
+};
 
 export const createGenerateFunction = (
   pulumiConfig: pulumi.Config,
@@ -9,14 +33,17 @@ export const createGenerateFunction = (
   serviceCloudScheduler: gcp.projects.Service,
   serviceBigQuery: gcp.projects.Service,
 ) => {
+  // Create a temporary directory to store the function code
+  // Google Cloud Functions struggles with bundling the internal packages, so this is a workaround
+  const tempDirectory = prepareTemporaryDirectory("generate", "../../apps/generate/dist");
+
   // Archive the function code in the bucket
   const functionBucketObject = new gcp.storage.BucketObject(
     `generate`, // Lowercase to avoid issues with GCS
     {
       bucket: functionAssetsBucket.name,
       source: new pulumi.asset.AssetArchive({
-        // TODO verify this path
-        ".": new pulumi.asset.FileArchive("../../apps/generate/dist/"),
+        ".": new pulumi.asset.FileArchive(tempDirectory),
       }),
     },
     {
@@ -115,7 +142,7 @@ export const createGetFunction = (
       bucket: functionAssetsBucket.name,
       source: new pulumi.asset.AssetArchive({
         // TODO verify this path
-        ".": new pulumi.asset.FileArchive("../../apps/get/dist/"),
+        ".": new pulumi.asset.FileArchive("../../apps/get/"),
       }),
     },
     {
