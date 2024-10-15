4fa4440a2a7b209b572582d3e14b3a76e781c250
Jem <0x0xjem@gmail.com>
2024-10-15 13:25:05 +0400

    get: Add typing and formatting to responses

--- apps/get/src/helpers/bigquery.ts
+++ apps/get/src/helpers/bigquery.ts
@@ -1,7 +1,9 @@
-import { BigQuery, BigQueryDate } from "@google-cloud/bigquery";
+import { BigQuery } from "@google-cloud/bigquery";
 import { getISO8601DateString } from "@repo/shared/date";
 import { getEnv } from "@repo/shared/env";
-import { logger, throwError } from "@repo/shared/logging";
+import { logger } from "@repo/shared/logging";
+import { parseNumber } from "@repo/shared/number";
+import { Snapshot } from "@repo/types";
 
 const performQuery = async (client: BigQuery, query: string) => {
   const [job] = await client.createQueryJob({
@@ -16,16 +18,7 @@ const performQuery = async (client: BigQuery, query: string) => {
   return rows;
 };
 
-const getDateValue = (date: unknown): string => {
-  if (typeof date !== "object" || date === null) {
-    throwError("object expected");
-  }
-
-  const bqDate = date as BigQueryDate;
-  return bqDate.value;
-};
-
-export const getSnapshots = async (startDate: Date, beforeDate: Date) => {
+export const getSnapshots = async (startDate: Date, beforeDate: Date): Promise<Snapshot[]> => {
   // Get the BigQuery details
   const gcpProject = getEnv("GCP_PROJECT");
   const bigQueryDataset = getEnv("BIGQUERY_DATASET");
@@ -41,21 +34,62 @@ export const getSnapshots = async (startDate: Date, beforeDate: Date) => {
   const bigQuery = new BigQuery({});
 
   // Get the snapshots
-  const snapshotRows = await performQuery(
+  // Skip the dt column, which is used for partitioning
+  const snapshotRows = (await performQuery(
     bigQuery,
     `
-    SELECT *
+    SELECT * EXCEPT (dt)
     FROM \`${gcpProject}.${bigQueryDataset}.${snapshotTable}\`
     WHERE dt >= '${getISO8601DateString(startDate)}' AND dt < '${getISO8601DateString(beforeDate)}'
   `,
-  );
+  )) as Snapshot[];
   logger.debug(`Fetched ${snapshotRows.length} snapshots`);
 
-  // Convert the dt column values to a date string
+  // All values are returned as strings
+  // Convert the number values to the correct type
   const snapshots = snapshotRows.map(row => {
+    const clearinghouses = row.clearinghouses.map(clearinghouse => {
+      return {
+        ...clearinghouse,
+        daiBalance: parseNumber(clearinghouse.daiBalance),
+        sDaiBalance: parseNumber(clearinghouse.sDaiBalance),
+        sDaiInDaiBalance: parseNumber(clearinghouse.sDaiInDaiBalance),
+        fundAmount: parseNumber(clearinghouse.fundAmount),
+        fundCadence: parseNumber(clearinghouse.fundCadence),
+      };
+    });
+
     return {
       ...row,
-      dt: getDateValue(row.dt),
+      clearinghouses,
+      clearinghouseTotals: {
+        daiBalance: parseNumber(row.clearinghouseTotals.daiBalance),
+        sDaiBalance: parseNumber(row.clearinghouseTotals.sDaiBalance),
+        sDaiInDaiBalance: parseNumber(row.clearinghouseTotals.sDaiInDaiBalance),
+      },
+      expiryBuckets: {
+        active: parseNumber(row.expiryBuckets.active),
+        expired: parseNumber(row.expiryBuckets.expired),
+        days30: parseNumber(row.expiryBuckets.days30),
+        days121: parseNumber(row.expiryBuckets.days121),
+      },
+      collateralDeposited: parseNumber(row.collateralDeposited),
+      collateralIncome: parseNumber(row.collateralIncome),
+      interestIncome: parseNumber(row.interestIncome),
+      interestReceivables: parseNumber(row.interestReceivables),
+      principalReceivables: parseNumber(row.principalReceivables),
+      terms: {
+        ...row.terms,
+        interestRate: parseNumber(row.terms.interestRate),
+        duration: parseNumber(row.terms.duration),
+        loanToCollateral: parseNumber(row.terms.loanToCollateral),
+      },
+      treasury: {
+        ...row.treasury,
+        daiBalance: parseNumber(row.treasury.daiBalance),
+        sDaiBalance: parseNumber(row.treasury.sDaiBalance),
+        sDaiInDaiBalance: parseNumber(row.treasury.sDaiInDaiBalance),
+      },
     };
   });
 
