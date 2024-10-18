fc9cb431184a61e63bfb6112cd8f45dad8653cd2
Jem <0x0xjem@gmail.com>
2024-10-18 15:22:50 +0400

    Add missing route for earliest snapshot

--- apps/get/src/index.ts
+++ apps/get/src/index.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express from "express";
 
-import { handleGetCurrentSnapshot, handleGetSnapshots } from "./snapshots";
+import { handleGetCurrentSnapshot, handleGetEarliestSnapshot, handleGetSnapshots } from "./snapshots";
 
 const app = express();
 
@@ -11,6 +11,7 @@ app.use(cors({ origin: true }));
 // Routes
 app.get("/snapshots", (req, res) => handleGetSnapshots(req, res));
 app.get("/snapshots/current", (req, res) => handleGetCurrentSnapshot(req, res));
+app.get("/snapshots/earliest", (req, res) => handleGetEarliestSnapshot(req, res));
 
 // Register the function
 // This matches the entry point defined for the cloud function
